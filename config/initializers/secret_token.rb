57900297adc73600630b53da74d803fe9578f40d
satindar <satindar@gmail.com>
2014-02-10 16:29:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeToRide::Application.config.secret_key_base = 'b9b16cf67b007214e0c2d297b853a3d14aa22c50208d89672a415f53514a73959487aa7a99f74e09147979feb05eb0e3ea1e9d7a9ca84cc70ce4323c3d43333e'
