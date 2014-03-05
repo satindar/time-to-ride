468718dff47fbf53802a34f9de76211faad096e0
satindar <satindar@gmail.com>
2014-03-04 16:34:14 -0800

    add figaro gem for env variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TimeToRide::Application.config.secret_key_base = 'b9b16cf67b007214e0c2d297b853a3d14aa22c50208d89672a415f53514a73959487aa7a99f74e09147979feb05eb0e3ea1e9d7a9ca84cc70ce4323c3d43333e'
+TimeToRide::Application.config.secret_key_base = ENV["SECRET_TOKEN"]
