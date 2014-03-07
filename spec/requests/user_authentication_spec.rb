1bf79cf145f53c4d1a3c939b60430872c368514b
satindar <satindar@gmail.com>
2014-03-06 23:41:37 -0800

    add factory for user and devise test support module

--- spec/requests/user_authentication_spec.rb
+++ spec/requests/user_authentication_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
-feature 'User can sign in and out' do
-  scenario 'User is not signed in' do
+feature 'User can see authentication links' do
+  scenario 'when not signed in' do
     visit root_path
 
     expect(page).to have_link('Login')
+  end
+end
+
+feature 'Sign in as a user' do
+  scenario 'with an email address' do
+    sign_in_as_a_valid_user
+
+    expect(page).to have_css '.notice', text: 'Signed in successfully'
   end
 
 end
